4250117a188c7bf622f6d80930472efd698f38f3
SirMoook <simiaty.8b@gmail.com>
2021-11-12 10:18:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+str1="Az alma az almafán terem"
+print ("A szöveg hossza:",len(str1))
+print (type(len(str1)))
+str2="terem"
+print(str1.find(str2))
+lower_str1=str1.lower()
+print(str1)
+print(lower_str1)
+print(str1.upper())
+print(lower_str1.islower())
